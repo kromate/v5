5c793f728be16ccacb20b2482667bebf7e2b99b1
kromate <akpananthony33@gmail.com>
2022-07-09 18:53:03 +0100

    feat: started hero section

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,59 @@
 <template>
 	<div class="h-[200vh]">
 		<div class="center h-screen w-full bg shadow-md">
-			<div class="line bg-[#ffffff7b] fixed h-0 w-0.5 z-10 md:left-16 left-5"/>
-			<div class="line bg-[#ffffff7b] fixed h-0 w-0.5 z-10 md:right-16 right-5"/>
-  <Loader  />
-  		</div>
+			<div class="line bg-[#ffffff] fixed h-0 w-px z-10 md:left-16 left-5" />
+			<div
+				class="line bg-[#ffffff] fixed h-0 w-px z-10 md:right-16 right-5"
+			/>
+
+			<Loader />
+
+			<div id="Hero">
+				<h1>Hi, my name is</h1>
+			</div>
+		</div>
 	</div>
 </template>
 
 <script setup lang="ts">
-import Loader from './components/Loader.vue'
+import Loader from './components/Loader.vue';
 </script>
 
 <style scoped>
-.line{
-    animation: grow 1.5s 1.5s 1 forwards;
+.line {
+	animation: grow 1.5s 1.5s 1 forwards;
 }
 .bg {
-	   background: #958e69;
-     transition: 0.8s ease-out;
+	background: #000;
+	transition: 0.8s ease-out;
 }
 .bg:before {
-     content: "";
-     position: absolute;
-     background: #212143;
-     bottom: 0;
-     left: 0;
-     right: 100%;
-     top: 0;
-     transition: top 0.65s ease-out;
-      animation: flow 1.5s 1 forwards;
+	content: '';
+	position: absolute;
+	background: #212143;
+	bottom: 0;
+	left: 0;
+	right: 100%;
+	top: 0;
+	transition: top 0.65s ease-out;
+	animation: flow 1.5s 1 forwards;
 }
-
+@keyframes grow {
+	from {
+		opacity: 0;
+		height: 0;
+	}
+	to {
+		opacity: 1;
+		height: 100%;
+	}
+}
+@keyframes flow {
+	from {
+		right: 100%;
+	}
+	to {
+		right: 0;
+	}
+}
 </style>
--- src/components/Loader.vue
+++ src/components/Loader.vue
@@ -17,10 +17,8 @@
 
 </script>
 
-<style >
-body{
-    background: #212143;
-}
+<style scoped>
+
 
 .logo-border {
 	animation: spin 1.5s 1;
@@ -30,19 +28,13 @@ body{
     animation: fade 0.5s 1.5s 1 forwards;
 }
 
-@keyframes grow {
-    from{opacity:0; height: 0;}
-    to{ opacity:1; height: 100%;}
-}
+
 @keyframes fade {
     from{opacity:1;}
     to{ opacity:0; display: none; transform: scale(0.2);}
 }
 
-@keyframes flow {
-    from{right: 100%;}
-    to{right: 0;}
-}
+
 
 @keyframes spin {
 	0% {transform: rotate(45deg) scale(1);}
