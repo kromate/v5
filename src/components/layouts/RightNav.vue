6698ec8efbb15209114bf390fcc2deddee6aa29e
kromate <akpananthony33@gmail.com>
2022-07-19 12:00:17 +0100

    added 404

--- src/components/layouts/RightNav.vue
+++ src/components/layouts/RightNav.vue
@@ -5,32 +5,34 @@
 	>
 		<ul class="gap-5 pt-2">
 			<li class="nav-link">
-				<a href="#about">About</a>
+				<a href="/#about">About</a>
 			</li>
 			<li class="nav-link">
-				<a href="#experience">Experience</a>
+				<a href="/#experience">Experience</a>
 			</li>
 			<li class="nav-link">
-				<a href="#works">Works</a>
+				<a href="/#works">Works</a>
 			</li>
 			<li class="nav-link">
-				<a href="#contact">Contact</a>
+				<a href="/#contact">Contact</a>
 			</li>
 		</ul>
 
 		<div class="mt-auto gap-8 flex-col flex">
-			<a class="nav-link" href="mailto:akpananthony33@gmail.com">akpananthony33@gmail.com</a>
+			<a class="nav-link" href="mailto:akpananthony33@gmail.com"
+				>akpananthony33@gmail.com</a
+			>
 		</div>
 	</nav>
 </template>
 
 <script setup lang="ts">
-import gsap from "gsap";
-import { onMounted } from "vue";
+import gsap from 'gsap';
+import { onMounted } from 'vue';
 
 onMounted(() => {
 	gsap.fromTo(
-		".nav-link",
+		'.nav-link',
 		{ opacity: 0, x: 20 },
 		{
 			opacity: 1,
@@ -38,7 +40,7 @@ onMounted(() => {
 			stagger: 0.2,
 			duration: 0.35,
 			delay: 1.75,
-			ease: "linear"
+			ease: 'linear',
 		}
 	);
 });
@@ -54,7 +56,7 @@ li {
 
 	writing-mode: vertical-rl;
 	a {
-		font-family: "Chakra Petch", sans-serif;
+		font-family: 'Chakra Petch', sans-serif;
 	}
 }
 li:not(:first-child) {
