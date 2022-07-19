a899bf0806d6b7be93648c37480f5da5bb9de592
kromate <akpananthony33@gmail.com>
2022-07-19 02:48:32 +0100

    changed fonts

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,11 +1,16 @@
 <template>
 	<section id="contact" class="text-secondary">
 		<h2 class="numbered-heading chakra" data-num="04.">Contact</h2>
-		<div class="flex-col center">
-			<p class="chakra text-center max-w-xl">
-				That all folks
+		<div class="flex-col center mt-44">
+			<p class="chakra text-center max-w-xl flex flex-col text-xl">
+				<span class="chakra">That all folks</span>
+
+				<br />
+				<span class="text-4xl md:text-6xl font-bold text-green chakra">Get in Touch</span>
 				<br />If you would love to work with me, collaborate on a project or just say "Hi"
-				<span>clicke the links on the sidebar</span>
+				<span
+					class="chakra"
+				>click the links on the sidebar</span>
 			</p>
 		</div>
 	</section>
