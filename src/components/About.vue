e2f8e89bebe5fa3ab9c733ffbdd9d6685fbc61bd
kromate <akpananthony33@gmail.com>
2022-07-20 04:11:33 +0100

    feat: update

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,4 +1,3 @@
-
 <template>
 	<section id="about">
 		<h2 class="numbered-heading chakra" data-num="01.">About Me</h2>
@@ -7,55 +6,69 @@
 			<div class="styledText">
 				<div>
 					<p>
-						Ohayo! 👋🏾 My name is Anthony Akpan. I am a Frontend Engineer who loves building crazy ideas and trying out new things.
-						I got into programming in 2017 when I found a Python e-book and then switched from Python to Web Development in 2018.
+						Ohayo! 👋🏾 My name is Anthony Akpan. I am a Frontend Engineer who
+						loves building crazy ideas and trying out new things. I got into
+						programming in 2017 when I found a Python e-book and then switched
+						from Python to Web Development in 2018.
 					</p>
 
 					<p>
-						Over the years, I have had the opportunity to work with amazing companies and people, and on diverse projects.
-						<br />I recently started a YouTube channel which is focused on documenting my Passion Projects (open source projects I work on) and my journey in tech.
-						<br />When I am not writing code, I am usually binging an anime/TV series.
-						<br />PS: I looove One Piece. It's all you want in an anime (interesting and non-ending😂).
+						Over the years, I have had the opportunity to work with amazing
+						companies and people, and on diverse projects.
+						<br />I recently started a YouTube channel which is focused on
+						documenting my Passion Projects (open source projects I work on) and
+						my journey in tech. <br />When I am not writing code, I am usually
+						binging an anime/TV series. <br />PS: I looove One Piece. It's all
+						you want in an anime (interesting and non-ending😂).
 					</p>
 				</div>
 
-				<p class="italic my-3 chakra">Below are a list of my tools and languages</p>
+				<p class="italic my-3 chakra">
+					Below are a list of my tools and languages
+				</p>
 				<ul class="flex gap-2.5 items-center mt-1.5 flex-wrap">
-					<li v-for="skill in skills" :key="skill" class="pill">{{skill}}</li>
+					<li v-for="skill in skills" :key="skill" class="pill">{{ skill }}</li>
 				</ul>
 			</div>
 
 			<div class="flex flex-col gap-4 mt-1 md:mt-0">
-				<img class="img w-[270px] rounded-md" src="../assets/images/me.png" alt="Headshot" />
-				<span
-					class="w-[270px] text-sm text-center"
-				>This bitmoji would have to do till I can take a proper headshot 😅</span>
+				<img
+					class="img w-[270px] rounded-md"
+					src="../assets/images/me.png"
+					alt="Headshot"
+				/>
+				<span class="w-[270px] text-sm text-center"
+					>This bitmoji would have to do till I can take a proper headshot
+					😅</span
+				>
 			</div>
 		</div>
 	</section>
 </template>
 
 <script setup lang="ts">
-import { ref } from "@vue/reactivity";
+import { ref } from '@vue/reactivity';
 
 const skills = ref([
-	"Javascript",
-	"Vue & Vuex",
-	"Ionic",
-	"React",
-	"Python",
-	"Tensorflow.js",
-	"Figma",
-	"Adobe XD",
-	"Android Studio",
-	"Firebase",
-	"Mongodb",
-	"WebSockets",
-	"WebRTC",
-	"Animations",
-	"APIs",
-	"plugins",
-	"extensions"
+	'Javascript',
+	'Vue & Vuex',
+	'Ionic',
+	'React',
+	'Python',
+	'Tensorflow.js',
+	'Figma',
+	'Adobe XD',
+	'Android Studio',
+	'Firebase',
+	'Mongodb',
+	'WebSockets',
+	'WebRTC',
+	'Animations',
+	'APIs',
+	'plugins',
+	'extensions',
+	'Testing (unit and E2E)',
+	'System Design',
 ]);
 </script>
 
