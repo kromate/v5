b3a40447418fedcc5872b1563e26e2054566480b
kromate <akpananthony33@gmail.com>
2022-07-24 03:58:02 +0100

    feat: show more/less button

--- src/composibles/projects.ts
+++ src/composibles/projects.ts
@@ -20,70 +20,6 @@ interface IProject {
 
 
 export const projectsArr: Array<IProject> = [
-
-    {
-        name: 'Script Runner',
-        desc: 'A chrome extension in which you can save Javascript code in the browser and run them later in the console',
-        tags: ['Chrome extension', 'Javascript'],
-        links: [{ name: 'github', url: 'https://github.com/kromate/chrome-extensions/tree/main/Script%20Runner' },],
-        show: true
-    },
-    {
-        name: 'Code Formatter',
-        desc: 'A chrome extension that helps you format your HTML & CSS code using prettier',
-        tags: ['Chrome extension', 'Javascript'],
-        links: [{ name: 'github', url: 'https://github.com/kromate/chrome-extensions/tree/main/Format%20Code' }, { name: 'live', url: 'https://chrome.google.com/webstore/detail/code-formatter/pbcmhfjddijdnkbfcbghfnoejdknfejd' },],
-        show: true
-    },
-    {
-        name: 'Vue3 Animation',
-        desc: 'A collection of vue 3 animations made with different libraries from GSAP to hover effects and just plain CSS animations',
-        tags: ['Vue', 'GSAP', 'Tailwind'],
-        links: [{ name: 'github', url: 'https://github.com/kromate/vue3-gsap-animations' }, { name: 'live', url: 'https://vue3-animations.netlify.app/' },],
-        show: true
-    },
-    {
-        name: 'Vue3 dashboard',
-        desc: 'I saw a very beautiful dashboard template in Vanilla Js and decided to recreate it in Vue',
-        tags: ['Vue', 'Tailwind', 'Chart.js'],
-        links: [{ name: 'github', url: 'https://github.com/kromate/vue3-vite-tailwind-dashboard' }, { name: 'live', url: 'https://vite-dashboard.netlify.app/' },],
-        show: true
-    },
-    {
-        name: 'LegalPreneur',
-        desc: 'The official website for Legalpreneur Attorneys & Consulting firm',
-        tags: ['Vue', 'Vue-draggable', 'Tailwind'],
-        links: [{ name: 'live', url: 'https://legalpreneurconsulting.com/' },],
-        show: true
-    },
-    {
-        name: 'Codeden',
-        desc: 'This is a drag n drop vue component builder with other feature like a component library and co',
-        tags: ['Vue', 'Vue-draggable', 'Tailwind'],
-        links: [{ name: 'github', url: 'https://github.com/kromate/Codeden' }, { name: 'live', url: 'https://codeden.netlify.app/' },],
-        show: true
-    },
-    {
-        name: 'Timelinx',
-        desc: 'I was bored and decided to implement a design from my friend',
-        tags: ['Nuxt', 'Tailwind', 'Firebase'],
-        links: [{ name: 'github', url: 'https://github.com/kromate/Timeline-Creator' }, { name: 'live', url: 'https://timelinx.netlify.app/' },],
-        show: true
-    },
-    {
-        name: 'Octobus',
-        desc: 'A web app in which users can get updates on buses and their schedules',
-        tags: ['Google Maps', 'Firebase', 'Node.Js'],
-        links: [{ name: 'github', url: 'https://github.com/kromate/Octobus' }, { name: 'live', url: 'https://octobus.netlify.app/' },],
-        show: true
-    },
-    {
-        name: 'Gamexit',
-        desc: 'A platform focused on connecting people using web based games. Create a 1v1 or tournament challenge and invite your friends ',
-        tags: ['Websocket', 'WebRTC', 'Node.Js'],
-        links: [{ name: 'github', url: 'https://github.com/kromate/gamexit' }, { name: 'live', url: 'https://gamexit.netlify.app/' },],
-        show: true
-    },
     {
         name: 'Wirepay',
         desc: 'I worked on the company`s website and user dashboard',
@@ -99,19 +35,86 @@ export const projectsArr: Array<IProject> = [
         show: true
     },
     {
-        name: 'AnimeDom',
-        desc: 'I was tired of the ads on popular anime websites so I built mine using puppeteer/cheerio to scrape data and Ionic for the frontend',
-        tags: ['Firebase', 'Puppeteer', 'Express'],
-        links: [{ name: 'github', url: 'https://github.com/kromate/AnimeDom-Ionic' }, { name: 'live', url: 'https://animedom.netlify.app/' },],
-        show: true
-    },
-    {
         name: 'Cloundenly',
         desc: 'I Worked on a suite of tools that enabled the the easy management of HR, Benefits and Payroll for Organisations',
         tags: ['Vue', 'Tailwind', 'Vuex'],
         links: [{ name: 'live', url: 'https://cloudenly.com/' },],
         show: true
     },
+    {
+        name: 'Gamexit',
+        desc: 'A platform focused on connecting people using web based games. Create a 1v1 or tournament challenge and invite your friends ',
+        tags: ['Websocket', 'WebRTC', 'Node.Js'],
+        links: [{ name: 'github', url: 'https://github.com/kromate/gamexit' }, { name: 'live', url: 'https://gamexit.netlify.app/' },],
+        show: true
+    },
+    {
+        name: 'Vue3 dashboard',
+        desc: 'I saw a very beautiful dashboard template in Vanilla Js and decided to recreate it in Vue',
+        tags: ['Vue', 'Tailwind', 'Chart.js'],
+        links: [{ name: 'github', url: 'https://github.com/kromate/vue3-vite-tailwind-dashboard' }, { name: 'live', url: 'https://vite-dashboard.netlify.app/' },],
+        show: true
+    },
+    {
+        name: 'Vue3 Animation',
+        desc: 'A collection of vue 3 animations made with different libraries from GSAP to hover effects and just plain CSS animations',
+        tags: ['Vue', 'GSAP', 'Tailwind'],
+        links: [{ name: 'github', url: 'https://github.com/kromate/vue3-gsap-animations' }, { name: 'live', url: 'https://vue3-animations.netlify.app/' },],
+        show: true
+    },
+    {
+        name: 'Codeden',
+        desc: 'This is a drag n drop vue component builder with other feature like a component library and co',
+        tags: ['Vue', 'Vue-draggable', 'Tailwind'],
+        links: [{ name: 'github', url: 'https://github.com/kromate/Codeden' }, { name: 'live', url: 'https://codeden.netlify.app/' },],
+        show: true
+    },
+    {
+        name: 'Script Runner',
+        desc: 'A chrome extension in which you can save Javascript code in the browser and run them later in the console',
+        tags: ['Chrome extension', 'Javascript'],
+        links: [{ name: 'github', url: 'https://github.com/kromate/chrome-extensions/tree/main/Script%20Runner' },],
+        show: true
+    },
+    {
+        name: 'Code Formatter',
+        desc: 'A chrome extension that helps you format your HTML & CSS code using prettier',
+        tags: ['Chrome extension', 'Javascript'],
+        links: [{ name: 'github', url: 'https://github.com/kromate/chrome-extensions/tree/main/Format%20Code' }, { name: 'live', url: 'https://chrome.google.com/webstore/detail/code-formatter/pbcmhfjddijdnkbfcbghfnoejdknfejd' },],
+        show: true
+    },
+    {
+        name: 'LegalPreneur',
+        desc: 'The official website for Legalpreneur Attorneys & Consulting firm',
+        tags: ['Vue', 'Vue-draggable', 'Tailwind'],
+        links: [{ name: 'live', url: 'https://legalpreneurconsulting.com/' },],
+        show: true
+    },
+
+    {
+        name: 'Timelinx',
+        desc: 'I was bored and decided to implement a design from my friend',
+        tags: ['Nuxt', 'Tailwind', 'Firebase'],
+        links: [{ name: 'github', url: 'https://github.com/kromate/Timeline-Creator' }, { name: 'live', url: 'https://timelinx.netlify.app/' },],
+        show: true
+    },
+    {
+        name: 'Octobus',
+        desc: 'A web app in which users can get updates on buses and their schedules',
+        tags: ['Google Maps', 'Firebase', 'Node.Js'],
+        links: [{ name: 'github', url: 'https://github.com/kromate/Octobus' }, { name: 'live', url: 'https://octobus.netlify.app/' },],
+        show: true
+    },
+
+
+    {
+        name: 'AnimeDom',
+        desc: 'I was tired of the ads on popular anime websites so I built mine using puppeteer/cheerio to scrape data and Ionic for the frontend',
+        tags: ['Firebase', 'Puppeteer', 'Express'],
+        links: [{ name: 'github', url: 'https://github.com/kromate/AnimeDom-Ionic' }, { name: 'live', url: 'https://animedom.netlify.app/' },],
+        show: true
+    },
+
     {
         name: 'Cloundenly-UI',
         desc: 'A list of Internal UI component used by different teams in Scelloo',
